3bdfbf2dbfd4f8c1a2c25ab7d377af570277cc35
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-01-13 17:47:17 +0530

    Animation effects

--- Connect-four/script.js
+++ Connect-four/script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-
     const squares = document.querySelectorAll('.grid div')
     const result = document.querySelector('#result')
     const displayCurrentPlayer = document.querySelector('#current-player')
