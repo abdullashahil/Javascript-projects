1eb4cb5155f251a337151a49245ddb57b804530d
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-01-13 17:46:36 +0530

    Audio effects

--- Connect-four/script.js
+++ Connect-four/script.js
@@ -117,21 +117,30 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < squares.length; i++) {
         squares[i].onclick = () => {
             if (squares[i + 7].classList.contains('taken') && !squares[i].classList.contains('taken')) {
+                const piece = document.createElement('div'); 
                 if (currentPlayer == 1) {
-                    squares[i].classList.add('taken')
-                    squares[i].classList.add('player-one')
-                    currentPlayer = 2
-                    displayCurrentPlayer.innerHTML = currentPlayer
+                    piece.classList.add('player-one');
                 } else if (currentPlayer == 2) {
-                    squares[i].classList.add('taken')
-                    squares[i].classList.add('player-two')
-                    currentPlayer = 1
-                    displayCurrentPlayer.innerHTML = currentPlayer
+                    piece.classList.add('player-two');
                 }
-            } else alert('cant go here')
-            checkBoard()
-        }
+    
+                squares[i].appendChild(piece);
+                piece.style.bottom = '100%';
+    
+                setTimeout(() => {
+                    piece.style.bottom = '0'; 
+                }, 10); 
+    
+                squares[i].classList.add('taken');
+                currentPlayer = currentPlayer === 1 ? 2 : 1;
+                displayCurrentPlayer.innerHTML = currentPlayer;
+            } else {
+                alert('cant go here');
+            }
+            checkBoard();
+        };
     }
+    
 
 
 
